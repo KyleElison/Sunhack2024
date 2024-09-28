0a5d3141cc6e182c25fdf0fc7760769fcd7c1534
Tobias Colvin <107777392+tobiasjdcolvin@users.noreply.github.com>
2024-09-28 14:19:01 -0700

    created sub-directory for app

--- frontend/src/views/app/App.js
+++ frontend/src/views/app/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Modal from "./components/Modal";
+import Modal from "../../components/Modal";
 import axios from "axios";
 
 class App extends Component {
@@ -101,9 +101,8 @@ class App extends Component {
         className="list-group-item d-flex justify-content-between align-items-center"
       >
         <span
-          className={`todo-title mr-2 ${
-            this.state.viewCompleted ? "completed-todo" : ""
-          }`}
+          className={`todo-title mr-2 ${this.state.viewCompleted ? "completed-todo" : ""
+            }`}
           title={item.description}
         >
           {item.title}
